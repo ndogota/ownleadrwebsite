ece860d12c4dff2818f4ff00245557a25b14669d
dogota_n <dogota_n@etna-alternance.net>
2020-05-22 11:43:37 +0200

    push count number

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ $(window).scroll(function() {
 
         {
 
-          duration: 2000,
+          duration: 3000,
           easing: 'swing',
           step: function() {
             $this.text(Math.floor(this.countNum));
